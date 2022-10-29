2ed0676996168027116fa30a6b21d745d05c10ff
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-10-29 15:11:31 +0300

    Redo task with async-await

---
+++ Asynchronous Programming/03.Gighub-Commits-async-await/app.js
@@ -0,0 +1,25 @@
+async function loadCommits() {
+  const username = document.querySelector(`#username`);
+  const repo = document.querySelector(`#repo`);
+  const ul = document.querySelector(`#commits`);
+  try {
+    const res = await fetch(
+      `https://api.github.com/repos/${username.value}/${repo.value}/commits`
+    );
+    if (res.ok == false) {
+      throw new Error(`Error ${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+
+    const list = data.map((res) => {
+      const li = document.createElement(`li`);
+      li.textContent = `${res.commit.author.name}: ${res.commit.message}`;
+      return li;
+    });
+    ul.replaceChildren(...list);
+  } catch (err) {
+    const li = document.createElement(`li`);
+    li.textContent = err.message;
+    ul.replaceChildren(li);
+  }
+}
