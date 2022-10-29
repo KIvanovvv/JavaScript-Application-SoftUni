4f48cc6747838461b1beee3c849ac1d4bed5d89c
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-10-29 13:37:14 +0300

    Add homework

---
+++ Asynchronous Programming/03.Gighub-Commits/app.js
@@ -0,0 +1,26 @@
+function loadCommits() {
+  const username = document.querySelector(`#username`);
+  const repo = document.querySelector(`#repo`);
+  const ul = document.querySelector(`#commits`);
+
+  fetch(`https://api.github.com/repos/${username.value}/${repo.value}/commits`)
+    .then((res) => {
+      if (res.ok == false) {
+        throw new Error(`Error ${res.status} ${res.statusText}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const list = data.map((res) => {
+        const li = document.createElement(`li`);
+        li.textContent = `${res.commit.author.name}: ${res.commit.message}`;
+        return li;
+      });
+      ul.replaceChildren(...list);
+    })
+    .catch((err) => {
+      const li = document.createElement(`li`);
+      li.textContent = err.message;
+      ul.replaceChildren(li);
+    });
+}
